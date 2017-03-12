cfaf3fd3c832ec5458bea7bbb7bc39ceef33392a
Graeme <graemebates10@gmail.com>
2017-03-11 16:07:43 -0800

    changed to placeholder

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html');
+    return render_template('index.html')
 
