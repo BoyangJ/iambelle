2cae9d4ef9e92eeb03852f1554b562771865f9ce
Boyang Jiao <boyang@uvic.ca>
2017-03-11 19:36:13 -0800

    added pulling local tweets, recent user tweets, replying to tweets, and favoriting tweets

--- helloworld.py
+++ helloworld.py
@@ -3,8 +3,8 @@
  
 import tweepy, time, sys
 import configparser
- 
-argfile = str(sys.argv[1])
+import code
+
 
 # get Twitter app auth info
 Config = configparser.ConfigParser()
@@ -17,14 +17,119 @@ ACCESS_SECRET = Config['AuthInfo']['ACCESS_SECRET']
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
- 
-filename=open(argfile,'r')
-f=filename.readlines()
-filename.close()
+
+user = api.get_user('iambellebot')
+
+
+def get_local_tweets():
+
+    victoria = api.reverse_geocode(48.4284, -123.3656)
+    victoria_id = victoria[0].id
+
+    query = "place:%s" % victoria_id
+    vic_tweets = [status for status in tweepy.Cursor(api.search, tweet_mode='extended', q=query).items(100)]
+
+    counter = 0
+
+    for status in reversed(vic_tweets):
+        if 'media' in status.entities:
+            if status.entities['media'][0]['type'] == 'photo':
+                print ("\nTEXT = ", status.full_text)
+                print ("ID = ", status.id)
+                print ("CREATED AT = ", status.created_at)
+                print ("SCREEN NAME = ", status.user.screen_name)
+                print ("url = twitter.com/statuses/", status.id, sep='')
+                print ("media_url = ", status.entities['media'][0]['media_url_https'])
+                counter = counter+1
+
+    print ("\n***** NUM PICTURES = %d *****\n" % counter)
+
+
+# retrieves a user's 20 most recent tweets
+# parameter u_id can either be a twitter @handle, or an id
+def get_user_tweets(u_id):
+
+    user_tweets = api.user_timeline(u_id, tweet_mode='extended');
+
+    print ("\n***** USER %s HAS %d TWEETS *****\n" % (u_id, len(user_tweets)))
+    
+    counter = 0
+
+    for status in reversed(user_tweets):
+        if 'media' in status.entities:
+            if status.entities['media'][0]['type'] == 'photo':
+                print ("\nTEXT = ", status.full_text)
+                print ("ID = ", status.id)
+                print ("CREATED AT = ", status.created_at)
+                print ("SCREEN NAME = ", status.user.screen_name)
+                print ("url = twitter.com/statuses/", status.id, sep='')
+                print ("media_url = ", status.entities['media'][0]['media_url_https'])
+                counter = counter+1
+
+    print ("\n***** NUM PICTURES = %d *****\n" % counter)
+
+
+# tweets a reply to tweet with id=t_id
+# parameter t_id must be a Twitter status id.
+def reply_to_tweet(t_id):
+    
+    screen_name = api.get_status(t_id, tweet_mode='extended').user.screen_name
+    print ("\nSCREEN NAME HERE = ", screen_name)
+    
+    reply_text = generate_reply(screen_name)
+    print ("\nREPLY TEXT HERE = ", reply_text)
+
+    api.create_favorite(t_id)
+    api.update_status(reply_text, t_id)
+
+
+
+# generates a tweet's text body
+# parameter u_name must be the Twitter @handle of the recipient
+def generate_reply(u_name):
+    return "@%s you are pretty ☺️" % u_name
 
 
 
 
-for line in f:
-    api.update_status(line)
-    time.sleep(900)#Tweet every 15 minutes
+
+get_user_tweets("iambellebot")
+#reply_to_tweet(840743579299414016)
+
+
+#filename=open(argfile,'r')
+#f=filename.readlines()
+#filename.close()
+
+
+
+#print ("\nENTITIES = ", vic_tweets[0].entities)
+#print ("\nTEXT = ", vic_tweets[0].full_text)
+#print ("ID = ", vic_tweets[0].id)
+
+
+#print (user['screen_name'])
+#print (api.get_status(123, tweet_mode='extended')['text'])
+
+
+#USE tweet_mode='extended' OR ELSE YOU LOSE
+
+#if 'media' in tweet.entities:
+#    tweet['entities']['media']
+
+
+
+#print (user.screen_name)
+#print (user.followers_count)
+#for friend in user.friends():
+#  print (friend.screen_name)
+   
+
+#for line in f:
+#    api.update_status(line)
+#    time.sleep(900)#Tweet every 15 minutes
+
+
+
+
+code.interact(local=locals())
