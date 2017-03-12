da209170c9df12358b51ba48f90d9a4ab786f20a
Graeme <graemebates10@gmail.com>
2017-03-11 23:32:30 -0800

    fixed tweets.py config vars

--- tweets.py
+++ tweets.py
@@ -4,6 +4,7 @@
 import tweepy, time, sys
 import configparser
 import code
+import os
 
 from image_rec import authorTweet
 
@@ -11,10 +12,10 @@ from image_rec import authorTweet
 Config = configparser.ConfigParser()
 Config.read("config.ini")
 
-CONSUMER_KEY = Config['AuthInfo']['CONSUMER_KEY']
-CONSUMER_SECRET = Config['AuthInfo']['CONSUMER_SECRET']
-ACCESS_KEY = Config['AuthInfo']['ACCESS_KEY']
-ACCESS_SECRET = Config['AuthInfo']['ACCESS_SECRET']
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_KEY = os.environ['ACCESS_KEY']
+ACCESS_SECRET = os.environ['ACCESS_SECRET']
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
